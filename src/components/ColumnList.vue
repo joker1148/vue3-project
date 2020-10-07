63a36c06fbaaf147412dc7b07088d9acbf129b0b
Joker1148 <zhangliming@coocaa.com>
2020-10-07 16:46:07 +0800

    init project

---
+++ src/components/ColumnList.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent,PropType } from "vue";
+export interface ColumnProps {
+    id: number;
+    title: string;
+    avatar: string;
+    description:string
+}
+export default defineComponent ({
+    name:'ColumnList',
+    props:{
+        list:{
+            type:Array as PropType<ColumnProps[]>, //将array的构造函数返回传入的泛型类型
+            required:true
+        }
+    },
+    setup(props){
+        props.list[0].
+    }
+})
+</script>
