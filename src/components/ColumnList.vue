00248c938f72577ffb55130ba9db5718d97f6624
Joker1148 <zhangliming@coocaa.com>
2020-10-07 19:40:13 +0800

    ColumnList init

--- src/components/ColumnList.vue
+++ src/components/ColumnList.vue
@@ -1,27 +1,54 @@
 <template>
-    <div>
-        
+  <div class="row">
+    <div v-for="column in columnList" :key="column.id" class="col-4 mb-4">
+      <div class="card h-100 shadow-sm">
+        <div class="card-body text-center">
+          <img :src="column.avatar " :alt="column.title" class="rounded-circle border border-light my-3">
+          <h5 class="card-title">{{column.title}}</h5>
+          <p class="card-text text-left">{{column.description}}</p>
+          <router-link :to="`/column/${column._id}`" class="btn btn-outline-primary">进入专栏</router-link>
+        </div>
+      </div>
     </div>
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent,PropType } from "vue";
+import {
+  defineComponent,
+  PropType
+  ,
+  computed
+} from 'vue'
+
 export interface ColumnProps {
     id: number;
     title: string;
-    avatar: string;
-    description:string
-}
-export default defineComponent ({
-    name:'ColumnList',
-    props:{
-        list:{
-            type:Array as PropType<ColumnProps[]>, //将array的构造函数返回传入的泛型类型
-            required:true
-        }
-    },
-    setup(props){
-        props.list[0].
+    avatar? : string;
+    description: string;
+  }
+export default defineComponent({
+  name: 'ColumnList',
+  props: {
+    list: {
+      type: Array as PropType < ColumnProps[] >, // 将array的构造函数返回传入的泛型类型
+      required: true
     }
+  },
+  setup (props) {
+    const columnList = computed(() => {
+      return props.list.map(column => {
+        console.log(column)
+        if (!column.avatar) {
+          column.avatar = require('@/assets/column.jpg')
+        }
+        return column
+      })
+    })
+    return {
+      columnList
+    }
+  }
 })
+
 </script>
