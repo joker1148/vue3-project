349e66d15afbdcbb74a1a9665387bd81352cf9c1
Joker1148 <zhangliming@coocaa.com>
2020-12-03 17:20:47 +0800

    test jenkins

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,7 @@
     <footer class="text-center py-4 text-secondary bg-light mt-6">
       <small>
         <ul class="list-inline mb-0">
-          <li class="list-inline-item">© 2020者也专栏</li>
+          <li class="list-inline-item">© 2020者也专栏1</li>
           <li class="list-inline-item">课程</li>
           <li class="list-inline-item">文档</li>
           <li class="list-inline-item">联系</li>
